84fab0da8538a53523b3a72d461bf59ae12812df
60734740 <jukka.aula@kolumbus.fi>
2022-05-02 13:54:51 +0300

    fontawesome-roskakori

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+//asennus: npm i --save @fortawesome/react-fontawesome@latest
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
