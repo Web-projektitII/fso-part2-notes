0af3487442c3accb2ce65248a049f033d0ed3678
60734740 <jukka.aula@kolumbus.fi>
2022-05-02 13:51:48 +0300

    fontawesome

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const Note = ({ note, toggleImportance, deleteNote }) => {
   const label = note.important
@@ -11,9 +13,10 @@ const Note = ({ note, toggleImportance, deleteNote }) => {
       <span>{note.content}</span>
       <span> ({note.lkm}) </span> 
       <button onClick={toggleImportance}>{label}</button>
-      <IconButton onClick={deleteNote} size="small" color="secondary">
+      {/*<IconButton onClick={deleteNote} size="small" color="secondary">
       <DeleteIcon />
-      </IconButton>
+      </IconButton> */}
+      <button onClick={deleteNote}><FontAwesomeIcon icon={faTrash}  color="red"/></button>
     </li>
   )
 }
